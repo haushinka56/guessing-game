5449e3636f331b6ba2fe34a4b203180f77df5307
anaBellomo <apaulabellomo@hotmail.com>
2023-02-02 13:49:31 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var secretNumber = parseInt(Math.random() * 1001);
+
+while (guess != secretNumber) {
+  var guess = prompt("Digite um número entre 1 e 1000");
+
+  if (guess == secretNumber) {
+    alert("Acertou!");
+  } else if (guess > secretNumber) {
+    alert("Errou :c \n O número secreto é menor que " + guess + ".");
+  } else if (guess < secretNumber) {
+    alert("Errou :c \n O número secreto é maior que" + guess + ".");
+  }
+}
