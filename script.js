33ebd7a4357b5b0862bd3b5b66a052c0991985aa
anaBellomo <apaulabellomo@hotmail.com>
2023-02-02 14:07:20 -0300

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var secretNumber = parseInt(Math.random() * 1001);
 
 while (guess != secretNumber) {
-  var guess = prompt("Digite um número entre 1 e 1000");
+  var guess = prompt("Adivinhe um número entre 1 e 1000");
 
   if (guess == secretNumber) {
     alert("Acertou!");
